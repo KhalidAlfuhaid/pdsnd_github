1366dce503b3b8ebe145ccaa3388ced08b4c1919
KhalidAlfuhaid <mids-ksa-12@hotmail.com>
2024-11-06 18:12:01 +0500

    refactor: Add input validation loop in get_filters function
    Refactored the get_filters function to include a while loop that
    handles invalid user inputs for city, month, and day selections.
    This enhancement ensures that only valid entries are accepted,
    improving the script's robustness and user experience.
    
     - Implemented while loop for input validation
     - Added prompts to guide users in case of invalid entries
    
    This update enhances input handling, making the program more
    reliable and user-friendly.

--- bikeshare.py
+++ bikeshare.py
@@ -16,16 +16,15 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
-    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+    while True:
+    # Added a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington)
 
     # get user input for month (all, january, february, ... , june)
 
-
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
-
-    print('-'*40)
+        print('-'*40)
     return city, month, day
 
 
